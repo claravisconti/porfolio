aa406ca3b594381541af4688a0b577313b6f35e5
claravisconti <mclaraviscontiu@gmail.com>
2026-04-26 19:02:17 -0300

    orden

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Services />
       <Typewriter />
       <PortfolioPreview/>
-      <Expertise/>
       <Testimonials/>
+      <Expertise/>
       <LargeTextSlider/>
     </>
   );
