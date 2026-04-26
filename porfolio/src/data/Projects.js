3795132ac75627ad2865b5f871d58b1f4f78fd25
claravisconti <mclaraviscontiu@gmail.com>
2026-04-25 22:49:16 -0300

    porfolio

--- porfolio/src/data/Projects.js
+++ porfolio/src/data/Projects.js
@@ -44,4 +44,82 @@ export const projects = [
       "https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=1200"
     ]
   },
+  {
+    id: 1,
+    title: "Bloom Indumentaria",
+    category: "Web",
+    image: Portada_Bloom,
+    slug: "bloom",
+    description: "Desarrollo de estrategia visual y gestión de contenidos para una tienda de diseño de indumentaria.",
+    gallery: [
+      "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?q=80&w=1200",
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1200",
+      "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=1200"
+    ]
+  },
+  {
+    id: 2,
+    title: "Payslip App",
+    category: "Web",
+    image: Portada_Payslip,
+    slug: "payslip",
+    description: "Web-app de recursos humanos y manejo de personal con ABM de documentación.",
+    gallery: [
+      "https://images.unsplash.com/photo-1487958449943-2429e8be8625?q=80&w=1200",
+      "https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?q=80&w=1200",
+      "https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=1200"
+    ]
+  },
+  {
+    id: 3,
+    title: "Fanfiction App",
+    category: "Web",
+    image: Portada_Fanfiction,
+    slug: "fanfiction",
+    description: "App de lectura de libros de ficción.",
+    gallery: [
+      "https://images.unsplash.com/photo-1487958449943-2429e8be8625?q=80&w=1200",
+      "https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?q=80&w=1200",
+      "https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=1200"
+    ]
+  },
+    {
+    id: 7,
+    title: "Bloom Indumentaria",
+    category: "Web",
+    image: Portada_Bloom,
+    slug: "bloom",
+    description: "Desarrollo de estrategia visual y gestión de contenidos para una tienda de diseño de indumentaria.",
+    gallery: [
+      "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?q=80&w=1200",
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1200",
+      "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=1200"
+    ]
+  },
+  {
+    id: 8,
+    title: "Payslip App",
+    category: "Web",
+    image: Portada_Payslip,
+    slug: "payslip",
+    description: "Web-app de recursos humanos y manejo de personal con ABM de documentación.",
+    gallery: [
+      "https://images.unsplash.com/photo-1487958449943-2429e8be8625?q=80&w=1200",
+      "https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?q=80&w=1200",
+      "https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=1200"
+    ]
+  },
+  {
+    id: 9,
+    title: "Fanfiction App",
+    category: "Web",
+    image: Portada_Fanfiction,
+    slug: "fanfiction",
+    description: "App de lectura de libros de ficción.",
+    gallery: [
+      "https://images.unsplash.com/photo-1487958449943-2429e8be8625?q=80&w=1200",
+      "https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?q=80&w=1200",
+      "https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=1200"
+    ]
+  },
 ];
