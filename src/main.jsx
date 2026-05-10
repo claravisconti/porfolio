a68e81d161ad679b44fef74714bf283121f3bca3
claravisconti <mclaraviscontiu@gmail.com>
2026-05-10 00:20:04 -0300

    rutas

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactGA.initialize("G-XSZFR9EYG3");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/porfolio">
       <App />
     </BrowserRouter >
   </React.StrictMode>,
