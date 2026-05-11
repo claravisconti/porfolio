9a4a89d1610b6c00aeead8d23bcfc64e97da45ba
claravisconti <mclaraviscontiu@gmail.com>
2026-05-10 21:02:31 -0300

    bundle

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,14 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AppRoutes from './routes';
 import ScrollToTopButton from './components/ScrollToTopButton';
-import { useEffect } from "react";
-import ReactGA from "react-ga4";
-import { clarity } from 'react-microsoft-clarity';
 
 function App() {
-  useEffect(() => {
-    // 1. Inicialización de Clarity
-    clarity.init("wotthqne6g");
-
-    // 2. Google Analytics Pageview
-    // Lo enviamos después de un pequeño delay para asegurar que el DOM esté listo
-    const timer = setTimeout(() => {
-      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-    }, 1000);
-
-    return () => clearTimeout(timer); // Limpieza del timer
-  }, []);
+  // Eliminamos el useEffect que inicializaba la analítica
+  // ahora el index.html se encargará de eso de forma diferida.
 
   return (
     <div className="min-h-screen flex flex-col font-sans">
       <Navbar />
-      {/* main con w-full asegura que los títulos gigantes lleguen a los bordes si es necesario */}
       <main className="flex-grow w-full overflow-x-hidden">
         <AppRoutes />
       </main>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter  } from 'react-router-dom' // <-- Importa esto
+import { BrowserRouter  } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import './i18n.js'
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-XSZFR9EYG3");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
